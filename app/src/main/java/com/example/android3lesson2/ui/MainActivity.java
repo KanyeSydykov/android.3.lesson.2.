c9e935a016dd436e80183927ffbe31c19927609a
user <skantoro@inbox.ru>
2021-06-21 11:53:13 +0600

    Initial commit

---
+++ app/src/main/java/com/example/android3lesson2/ui/MainActivity.java
@@ -0,0 +1,19 @@
+package com.example.android3lesson2.ui;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.android3lesson2.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+
+    }
+}
